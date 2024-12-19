b33a475cdd6cf2dc3ed635288af109c95cccb01f
ThilinaKaushaka <thilinaw72@gmail.com>
2024-12-19 17:56:59 +0530

    add scroll bar

--- javaScript/script.js
+++ javaScript/script.js
@@ -20,9 +20,19 @@ function sendMSG() {
     if($('.text').val()!=""){
         selectUser();
         if (user=="saman") {
-            $('.c').append(`<p class="left">${$('.text').val()}</p>`);
+            
+            $('.c').append(`
+                <div class="row">
+                    <p class="left">${$('.text').val()}</p>
+                </div>
+            `);
         }else{
-            $('.c').append(`<p class="right">${$('.text').val()}</p>`);
+            $('.c').append(`
+            <div class="row">
+                <p class="right">${$('.text').val()}</p>
+                
+            </div>
+            `);
         }
     }
 
